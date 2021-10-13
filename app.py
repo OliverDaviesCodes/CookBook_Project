12a5ab8d675c6fc65c8b3fc0c01afdbe597ab935
OliverDaviesCodes <oliverdavieswork@gmail.com>
2021-10-13 08:55:59 +0000

    Add edit buttons for recipes

--- app.py
+++ app.py
@@ -46,7 +46,6 @@ def register():
         session["user"] = request.form.get("username").lower()
         flash("Registration Succesful")
         return redirect(url_for("profile", username=session["user"]))
-
     return render_template("register.html")
 
 
