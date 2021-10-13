1332824dba10ec93178234a7af2303e81f89ad97
OliverDaviesCodes <oliverdavieswork@gmail.com>
2021-10-13 10:09:07 +0000

    update recipes into the database

--- app.py
+++ app.py
@@ -116,8 +116,20 @@ def add_recipe():
 
 @app.route("/edit_recipe/<recipe_id>", methods=["GET", "POST"])
 def edit_recipe(recipe_id):
-    recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
 
+    if request.method == "POST":
+        is_vegetarian = "on" if request.form.get("is_vegetarian") else "off"
+        submit = {
+            "category_name": request.form.get("category_name"),
+            "recipe_name": request.form.get("recipe_name"),
+            "recipe_description": request.form.get("recipe_description"),
+            "is_vegetarian": is_vegetarian,
+            "created_by": session["user"]
+        }
+        mongo.db.tasks.update({"_id": ObjectId(recipe_id)}, submit)
+        flash("Recipe Successfully Updated")
+
+    recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template(
         "edit_recipes.html", recipe=recipe, categories=categories)
