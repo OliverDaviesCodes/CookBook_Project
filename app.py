6b60652d0940991c0d302bcb4857c9df428686e9
OliverDaviesCodes <oliverdavieswork@gmail.com>
2021-10-15 07:38:19 +0000

    Deleting recipes from the database

--- app.py
+++ app.py
@@ -134,6 +134,13 @@ def edit_recipe(recipe_id):
     return render_template(
         "edit_recipes.html", recipe=recipe, categories=categories)
 
+
+@app.route("/delete_recipe/<recipe_id>")
+def delete_recipe(recipe_id):
+    mongo.db.recipes.remove({"_id": ObjectId(recipe_id)})
+    flash("Recipe Successfully Deleted")
+    return redirect(url_for("get_recipes"))
+
 # This tells the app how and where to run the application
 
 
